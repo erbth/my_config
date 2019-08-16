b0e10c9db532065a2ac7730e88508a9a60f230ec
erbth <t.erbesdobler@gmx.de>
2019-08-16 18:14:44 +0200

    More dot files and script

---
+++ create_links.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for LINK in *
+do
+	if [ "$LINK" == "create_links.sh" ]
+	then
+		continue
+	fi
+
+	from="$PWD/$LINK"
+	to="$HOME/.$LINK"
+
+	if [ ! -L "$to" ]
+	then
+		if [ -e "$to" ]
+		then
+			rm -i "$to"
+		fi
+
+		echo "$from --> $to"
+		ln -s "$from" "$to" 
+	fi
+done
