f65a09d6e0959952362c4a57da5e76ad67d638c6
erbth <t.erbesdobler@gmx.de>
2025-05-14 19:55:57 +0200

    ...

--- create_links.sh
+++ create_links.sh
@@ -1,20 +1,50 @@
 #!/usr/bin/env bash
 
-for LINK in *
+BASENAME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+if [ -z "$HOME" ]
+then
+	exit 2
+fi
+
+
+for LINK in "$BASENAME"/*
 do
-	if [ "$LINK" == "create_links.sh" ]
+	if [ "$LINK" == "$BASENAME/create_links.sh" ]
 	then
 		continue
 	fi
 
-	from="$PWD/$LINK"
-	to="$HOME/.$LINK"
+	if [ ! -f $LINK ]
+	then
+		continue
+	fi
+
+	from="$LINK"
+	to="$HOME/.$(basename "$LINK")"
 
 	if [ ! -L "$to" ]
 	then
 		if [ -e "$to" ]
 		then
 			rm -i "$to"
+		fi
+
+		echo "$from --> $to"
+		ln -s "$from" "$to" 
+	fi
+done
+
+for LINK in "$BASENAME/config"/*
+do
+	from="$LINK"
+	to="$HOME/.config/$(basename "$LINK")"
+
+	if [ ! -L "$to" ]
+	then
+		if [ -e "$to" ]
+		then
+			rm -ri "$to"
 		fi
 
 		echo "$from --> $to"
